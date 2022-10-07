956840ed2ea1eb6dd53a52efef78b309e2c466dc
ilya-kas <ilya02.mail@gmail.com>
2022-10-07 12:58:14 +0300

    rearrange: remove builded files
    Remove builded files to decrease project's size

--- ChemistryARUnity/ExportedApp/unityLibrary/src/main/Il2CppOutputProject/Source/il2cppOutput/lumpedcpp/Lump_libil2cpp_codegen.cpp
+++
@@ -1,4 +0,0 @@
-#include "il2cpp-config.h"
-#include "D:\Projects\Unity\ChemistryAR\ChemistryARUnity\ExportedApp\unityLibrary\src\main\Il2CppOutputProject\IL2CPP\libil2cpp\codegen\il2cpp-codegen-common.cpp"
-#include "D:\Projects\Unity\ChemistryAR\ChemistryARUnity\ExportedApp\unityLibrary\src\main\Il2CppOutputProject\IL2CPP\libil2cpp\codegen\il2cpp-codegen-il2cpp.cpp"
-#include "D:\Projects\Unity\ChemistryAR\ChemistryARUnity\ExportedApp\unityLibrary\src\main\Il2CppOutputProject\IL2CPP\libil2cpp\codegen\il2cpp-codegen-tiny.cpp"
--- ChemistryARUnity/ExportedApp/unityLibrary/src/main/Il2CppOutputProject/Source/il2cppOutput/lumpedcpp/Lump_libil2cpp_mono.cpp
+++
@@ -1,7 +0,0 @@
-#include "il2cpp-config.h"
-#include "D:\Projects\Unity\ChemistryAR\ChemistryARUnity\ExportedApp\unityLibrary\src\main\Il2CppOutputProject\IL2CPP\libil2cpp\mono\MonoPosixHelper.cpp"
-#include "D:\Projects\Unity\ChemistryAR\ChemistryARUnity\ExportedApp\unityLibrary\src\main\Il2CppOutputProject\IL2CPP\libil2cpp\mono\ThreadPool\ThreadPoolMonitorThread.cpp"
-#include "D:\Projects\Unity\ChemistryAR\ChemistryARUnity\ExportedApp\unityLibrary\src\main\Il2CppOutputProject\IL2CPP\libil2cpp\mono\ThreadPool\ThreadPoolWorkerThread.cpp"
-#include "D:\Projects\Unity\ChemistryAR\ChemistryARUnity\ExportedApp\unityLibrary\src\main\Il2CppOutputProject\IL2CPP\libil2cpp\mono\ThreadPool\threadpool-ms-io-poll.cpp"
-#include "D:\Projects\Unity\ChemistryAR\ChemistryARUnity\ExportedApp\unityLibrary\src\main\Il2CppOutputProject\IL2CPP\libil2cpp\mono\ThreadPool\threadpool-ms-io.cpp"
-#include "D:\Projects\Unity\ChemistryAR\ChemistryARUnity\ExportedApp\unityLibrary\src\main\Il2CppOutputProject\IL2CPP\libil2cpp\mono\ThreadPool\threadpool-ms.cpp"
